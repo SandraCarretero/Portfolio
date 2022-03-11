351635abd15946268e73b67557433eb3f9177912
SandraCarretero <sandracarretero24@gmail.com>
2022-03-11 11:52:00 +0100

    scroll lateral con js

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+$(function(){
+    $(window).scroll( function(){
+        var scroll = $(window).scrollTop();
+        // console.log("scroll"+scroll);
+        
+        var distancia_elemento = $(".about").offset().top;
+        // console.log("distancia elemento"+distancia_elemento);
+
+        var max = $(window).width();
+
+        var move = distancia_elemento -scroll;
+
+        if(scroll >= distancia_elemento && scroll < distancia_elemento + 2*max ){
+            console.log("Estoy aquí")
+            $(".line").css({
+                "transform": "translateX("+move+"px)"
+            });
+        }
+
+        else{
+            $(".line").css({
+                "transform": "translateX(0px)"
+            });
+        }
+    });
+});
